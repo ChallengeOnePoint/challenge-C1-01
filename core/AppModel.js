1f904161be12937383083058bfd4f0bf91b52f94
Dammmien <d.damien@live.fr>
2016-01-23 12:03:45 +0100

    core css/js and build

--- core/AppModel.js
+++ core/AppModel.js
@@ -2,7 +2,22 @@ app.factory( 'AppModel', function() {
 
     return {
 
-        example: null
+        adresses: [ {
+            number: "1",
+            street: "Rue Saint-Laurent",
+            city: "Paris 10e Arrondissement",
+            postcode: "75010",
+            firstname: "Aaron",
+            lastname: "Desamparo"
+        }, {
+            number: "123",
+            street: "Rue Du Faubourg Saint-Martin",
+            city: "Paris 10e Arrondissement",
+            postcode: "75010",
+            firstname: "Abbey",
+            lastname: "Desan"
+        } ]
+
 
     };
 
